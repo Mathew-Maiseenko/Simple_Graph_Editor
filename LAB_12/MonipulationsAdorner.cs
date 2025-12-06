74a5a07eda36b9034ef2a89564d32c3bcdfed8f2
Mathew Maiseenko <maislov.alex@gmail.com>
2025-12-06 17:50:52 +0300

    fix: let polygon and bezier move and rotate correctly

--- LAB_12/MonipulationsAdorner.cs
+++ LAB_12/MonipulationsAdorner.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
@@ -17,11 +18,29 @@ namespace LAB_12
         private VisualCollection visualChildren;
         private Thumb moveThumb, resizeThumb, rotateThumb;
         private Shape adornedShape;
+        private TransformGroup transformGroup;
+        private RotateTransform rotateTransform;
+        private ScaleTransform scaleTransform;
+
+        // Для вращения мышью
+        private Point startMousePosition;
+        private double startAngle;
+        private bool isRotating = false;
 
         public MonipulationsAdorner(UIElement adornedElement) : base(adornedElement)
         {
             adornedShape = adornedElement as Shape;
             visualChildren = new VisualCollection(this);
+
+            rotateTransform = new RotateTransform();
+            scaleTransform = new ScaleTransform();
+            transformGroup = new TransformGroup();
+
+            transformGroup.Children.Add(scaleTransform);
+            transformGroup.Children.Add(rotateTransform);
+
+            adornedShape.RenderTransform = transformGroup;
+            adornedShape.RenderTransformOrigin = new Point(0.5, 0.5); // Устанавливаем центр трансформации
 
             // Перемещение
             moveThumb = BuildThumb(Brushes.LightBlue);
@@ -31,11 +50,17 @@ namespace LAB_12
             // Масштабирование
             resizeThumb = BuildThumb(Brushes.LightGreen);
             resizeThumb.DragDelta += ResizeThumb_DragDelta;
+            resizeThumb.DragCompleted += Transform_DragCompleted;
             visualChildren.Add(resizeThumb);
 
             // Поворот
             rotateThumb = BuildThumb(Brushes.Orange);
+
+            // Используем старый способ через DragDelta для простоты
             rotateThumb.DragDelta += RotateThumb_DragDelta;
+            rotateThumb.DragCompleted += Transform_DragCompleted;
+
+            rotateThumb.Cursor = Cursors.Hand;
             visualChildren.Add(rotateThumb);
         }
 
@@ -53,7 +78,6 @@ namespace LAB_12
         // Сдвиг
         private void MoveThumb_DragDelta(object sender, DragDeltaEventArgs e)
         {
-            // для всех фигур (Ellipse, Line, Polygon и т.д.)
             double left = Canvas.GetLeft(adornedShape);
             double top = Canvas.GetTop(adornedShape);
 
@@ -67,20 +91,218 @@ namespace LAB_12
         // Масштабирование
         private void ResizeThumb_DragDelta(object sender, DragDeltaEventArgs e)
         {
-            adornedShape.RenderTransform = new ScaleTransform(
-                1 + e.HorizontalChange / 100.0,
-                1 + e.VerticalChange / 100.0,
-                adornedShape.RenderSize.Width / 2,
-                adornedShape.RenderSize.Height / 2);
+            double newScaleX = scaleTransform.ScaleX + e.HorizontalChange / 100.0;
+            double newScaleY = scaleTransform.ScaleY + e.VerticalChange / 100.0;
+
+            if (newScaleX > 0)
+                scaleTransform.ScaleX = newScaleX;
+
+            if (newScaleY > 0)
+                scaleTransform.ScaleY = newScaleY;
         }
 
-        // Поворот
+        // Вращение
         private void RotateThumb_DragDelta(object sender, DragDeltaEventArgs e)
         {
-            adornedShape.RenderTransform = new RotateTransform(
-                e.HorizontalChange, // угол поворота
-                adornedShape.RenderSize.Width / 2,
-                adornedShape.RenderSize.Height / 2);
+            rotateTransform.Angle += e.HorizontalChange;
+            // Центр вращения уже установлен через RenderTransformOrigin
+        }
+
+        // После завершения вращения или масштабирования
+        private void Transform_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            ApplyTransformationsAndReset();
+        }
+
+        // Метод для применения трансформаций к фигуре и сброса состояния
+        private void ApplyTransformationsAndReset()
+        {
+            // Получаем матрицу трансформации
+            Matrix transformMatrix = adornedShape.RenderTransform.Value;
+
+            // Если есть трансформации (вращение/масштабирование)
+            if (!transformMatrix.IsIdentity)
+            {
+                // Получаем текущую позицию на Canvas
+                double currentLeft = Canvas.GetLeft(adornedShape);
+                double currentTop = Canvas.GetTop(adornedShape);
+
+                if (double.IsNaN(currentLeft)) currentLeft = 0;
+                if (double.IsNaN(currentTop)) currentTop = 0;
+
+                // Применяем трансформацию к геометрии фигуры
+                ApplyTransformationToShapeGeometry(transformMatrix);
+
+                // Сбрасываем трансформации RenderTransform
+                ResetTransformations();
+            }
+        }
+
+        // Применяет трансформацию к геометрии фигуры
+        private void ApplyTransformationToShapeGeometry(Matrix matrix)
+        {
+            // Получаем размеры фигуры
+            double width = adornedShape.ActualWidth;
+            double height = adornedShape.ActualHeight;
+
+            // Центр фигуры в локальных координатах
+            Point localCenter = new Point(width / 2, height / 2);
+
+            // Создаем матрицу трансформации относительно центра фигуры
+            Matrix centeredMatrix = matrix;
+
+            // Трансформируем относительно центра (смещаем в центр, применяем, смещаем обратно)
+            Matrix translateToCenter = Matrix.Identity;
+            translateToCenter.Translate(-localCenter.X, -localCenter.Y);
+
+            Matrix translateBack = Matrix.Identity;
+            translateBack.Translate(localCenter.X, localCenter.Y);
+
+            centeredMatrix = Matrix.Multiply(translateToCenter, centeredMatrix);
+            centeredMatrix = Matrix.Multiply(centeredMatrix, translateBack);
+
+            // Применяем в зависимости от типа фигуры
+            if (adornedShape is Path path && path.Data is Geometry geometry)
+            {
+                // Для Path с Geometry
+                ApplyTransformationToGeometry(geometry, centeredMatrix);
+            }
+            else if (adornedShape is Ellipse ellipse)
+            {
+                // Для Ellipse создаем Path с EllipseGeometry
+                ConvertToPathAndApplyTransformation(ellipse, centeredMatrix);
+            }
+            else if (adornedShape is Rectangle rectangle)
+            {
+                // Для Rectangle создаем Path с RectangleGeometry
+                ConvertToPathAndApplyTransformation(rectangle, centeredMatrix);
+            }
+            else if (adornedShape is Polygon polygon)
+            {
+                // Для Polygon трансформируем точки относительно локального центра
+                TransformPolygonPoints(polygon, centeredMatrix);
+            }
+            else if (adornedShape is Polyline polyline)
+            {
+                // Для Polyline трансформируем точки относительно локального центра
+                TransformPolylinePoints(polyline, centeredMatrix);
+            }
+        }
+
+        // Применяет трансформацию к Geometry
+        private void ApplyTransformationToGeometry(Geometry geometry, Matrix matrix)
+        {
+            if (geometry.Transform is TransformGroup transformGroup)
+            {
+                // Добавляем новую трансформацию к существующим
+                MatrixTransform matrixTransform = new MatrixTransform(matrix);
+                transformGroup.Children.Add(matrixTransform);
+            }
+            else if (geometry.Transform is MatrixTransform existingTransform)
+            {
+                // Комбинируем с существующей трансформацией
+                Matrix combinedMatrix = Matrix.Multiply(existingTransform.Matrix, matrix);
+                geometry.Transform = new MatrixTransform(combinedMatrix);
+            }
+            else
+            {
+                // Создаем новую трансформацию
+                geometry.Transform = new MatrixTransform(matrix);
+            }
+        }
+
+        // Конвертирует простую фигуру в Path и применяет трансформацию
+        private void ConvertToPathAndApplyTransformation(Shape originalShape, Matrix matrix)
+        {
+            Geometry geometry = null;
+
+            if (originalShape is Ellipse ellipse)
+            {
+                // Создаем EllipseGeometry
+                geometry = new EllipseGeometry(new Rect(0, 0, ellipse.ActualWidth, ellipse.ActualHeight));
+            }
+            else if (originalShape is Rectangle rectangle)
+            {
+                // Создаем RectangleGeometry
+                geometry = new RectangleGeometry(new Rect(0, 0, rectangle.ActualWidth, rectangle.ActualHeight));
+            }
+
+            if (geometry != null)
+            {
+                // Применяем трансформацию
+                geometry.Transform = new MatrixTransform(matrix);
+
+                // Создаем новую Path
+                Path newPath = new Path
+                {
+                    Data = geometry,
+                    Fill = originalShape.Fill,
+                    Stroke = originalShape.Stroke,
+                    StrokeThickness = originalShape.StrokeThickness,
+                    StrokeDashArray = originalShape.StrokeDashArray
+                };
+
+                // Заменяем в родительском контейнере
+                if (originalShape.Parent is Panel parentPanel)
+                {
+                    // Сохраняем позицию
+                    double left = Canvas.GetLeft(originalShape);
+                    double top = Canvas.GetTop(originalShape);
+
+                    int index = parentPanel.Children.IndexOf(originalShape);
+                    parentPanel.Children.Remove(originalShape);
+                    parentPanel.Children.Insert(index, newPath);
+
+                    // Восстанавливаем позицию
+                    if (!double.IsNaN(left))
+                        Canvas.SetLeft(newPath, left);
+                    if (!double.IsNaN(top))
+                        Canvas.SetTop(newPath, top);
+
+                    // Обновляем ссылку на фигуру
+                    adornedShape = newPath;
+                }
+            }
+        }
+
+        // Трансформирует точки Polygon
+        private void TransformPolygonPoints(Polygon polygon, Matrix matrix)
+        {
+            if (polygon.Points == null || polygon.Points.Count == 0) return;
+
+            PointCollection transformedPoints = new PointCollection();
+            foreach (Point point in polygon.Points)
+            {
+                Point transformedPoint = matrix.Transform(point);
+                transformedPoints.Add(transformedPoint);
+            }
+
+            // Заменяем исходную коллекцию
+            polygon.Points = transformedPoints;
+        }
+
+        // Трансформирует точки Polyline
+        private void TransformPolylinePoints(Polyline polyline, Matrix matrix)
+        {
+            if (polyline.Points == null || polyline.Points.Count == 0) return;
+
+            PointCollection transformedPoints = new PointCollection();
+            foreach (Point point in polyline.Points)
+            {
+                Point transformedPoint = matrix.Transform(point);
+                transformedPoints.Add(transformedPoint);
+            }
+
+            // Заменяем исходную коллекцию
+            polyline.Points = transformedPoints;
+        }
+
+        // Сбрасывает трансформации RenderTransform
+        private void ResetTransformations()
+        {
+            scaleTransform.ScaleX = 1;
+            scaleTransform.ScaleY = 1;
+            rotateTransform.Angle = 0;
         }
 
         protected override Size ArrangeOverride(Size finalSize)
@@ -97,6 +319,5 @@ namespace LAB_12
 
         protected override int VisualChildrenCount => visualChildren.Count;
         protected override Visual GetVisualChild(int index) => visualChildren[index];
-
     }
 }
